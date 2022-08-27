0cdfa2fa37a1d63e1040b9edfe2fdd854a6aba49
Caio Andrade <caioandradesje@gmail.com>
2022-08-27 17:00:42 -0300

    aula 2 variaveis

---
+++ Aula 2 - variaveis/script.js
@@ -0,0 +1,98 @@
+// tipos primitivos
+
+// boolean
+// var vOUf = false;
+// console.log(typeof (vOUf));
+
+// numero
+// var numeroQualquer = 1;
+// console.log(typeof (numeroQualquer));
+
+// string
+// var nome = 'Caio';
+// console.log(typeof (nome));
+
+// // Function
+// var funcao = function () { }
+
+// como declarar
+// var variavel = 'Caio';
+// variavel = 'oiaC';
+// console.log(variavel);
+
+// let variavel_Sim = 'diana';
+
+// console.log(variavel_Sim);
+
+// Constante
+// const constante = 'caio';
+// console.log(constante);
+
+// var escopoGlobal = 'Global';
+// console.log(escopoGlobal);
+
+// function escopoLocal() {
+//     let escopoLocalInterno = 'local';
+//     console.log(escopoLocalInterno);
+// }
+// escopoLocal();
+
+// var atribuicao = 'Nome';
+
+// comparacao
+// var comparacao = '0' == 0;
+// console.log(comparacao);
+
+// compara identica
+var comparacaoIdentica = '0' === 0;
+console.log(comparacaoIdentica);
+
+// adicao
+var adicao = 1 + 1;
+console.log(adicao);
+
+// subtracao
+var subtrcao = 2 - 1;
+console.log(subtrcao);
+
+//  multiplicacao
+var multiplicacao = 2 * 3;
+console.log(multiplicacao);
+
+// divisao real
+var divisaoReal = 6 / 2;
+console.log(divisaoReal);
+
+// divisao inteira
+var divisaoInteira = 5 % 2;
+console.log(divisaoInteira);
+
+// potenciacao
+var potenciacao = 2 ** 10;
+console.log(potenciacao);
+
+// maior que
+var maiorQue = 5 > 2;
+console.log(maiorQue);
+
+// menor que 
+var menorQue = 5 < 2;
+console.log(menorQue);
+
+// maior ou igual a
+var maiorOuIgual = 5 >= 2;
+console.log(maiorOuIgual);
+
+// menor ou igual a
+var menorOuIgual = 5 <= 2;
+console.log(menorOuIgual);
+
+// 
+var e = true && false;
+console.log(e);
+
+var ou = true || false;
+console.log(ou);
+
+var nao = !true;
+console.log(nao);
